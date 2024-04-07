87373d42cdb3d38d6397f3e3fcb602c64edf2a07
Arpit Das <arpitdas118@gmail.com>
2024-04-07 23:18:00 +0530

    create base layout for dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Grid, GridItem, ResponsiveValue, Text } from "@chakra-ui/react";
+import BodySection from "../components/BodySection";
+import SidePanel from "../components/SidePanel";
+
+const templateAreas: ResponsiveValue<string> = `"sidePanel body"`;
+const templateRows: ResponsiveValue<string> = "repeat(2, 100%)";
+const templateColumns: ResponsiveValue<string> = "15rem 1fr";
 
 const Dashboard = () => {
-  return <div>2z</div>;
+  return (
+    <Grid
+      w="100vw"
+      h="100vh"
+      templateAreas={templateAreas}
+      templateRows={templateRows}
+      templateColumns={templateColumns}
+    >
+      <GridItem area={"sidePanel"}>
+        <SidePanel />
+      </GridItem>
+
+      <GridItem area={"body"} py="10" pr="10">
+        <BodySection />
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default Dashboard;
