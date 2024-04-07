afc379b99d1642330d701f2501ac4031b38d938f
Arpit Das <arpitdas118@gmail.com>
2024-04-07 23:44:25 +0530

    add border radius to body section component

--- src/components/BodySection.tsx
+++ src/components/BodySection.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@chakra-ui/react";
 
 const BodySection = () => {
-  return <Box bgColor="gray.700" w="100%" h="100%"></Box>;
+  return <Box bgColor="gray.700" w="100%" h="100%" borderRadius="0.4rem"></Box>;
 };
 
 export default BodySection;
