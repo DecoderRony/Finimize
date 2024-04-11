500a94eaca5aaeb23b1c681dd8f456d3c89630ab
Arpit Das <arpitdas118@gmail.com>
2024-04-12 02:31:33 +0530

    create constants for routes

--- src/constants.tsx
+++ src/constants.tsx
@@ -6,6 +6,13 @@ import {
   BsTelephone,
 } from "react-icons/bs";
 
+export const DASHBOARD_ROUTE = "dashboard";
+export const EXPENSES_ROUTE = "expenses";
+export const SETTINGS_ROUTE = "settings";
+export const SUPPORT_ROUTE = "support";
+export const LOGIN_ROUTE = "login";
+export const ADD_EXPENSE_ROUTE = "add-expense";
+
 export interface PanelItem {
   displayName: string;
   id: string;
@@ -18,24 +25,24 @@ export const sidePanelItems: PanelItem[] = [
     displayName: "Dashboard",
     id: "dashboard",
     icon: BsHouse,
-    route: "/dashboard",
+    route: "/",
   },
   {
     displayName: "Expenses",
     id: "expenses",
     icon: BsCreditCard2Back,
-    route: "expenses",
+    route: EXPENSES_ROUTE,
   },
   {
     displayName: "Settings",
     id: "settings",
     icon: BsGearWideConnected,
-    route: "settings",
+    route: SETTINGS_ROUTE,
   },
   {
     displayName: "Support",
     id: "support",
     icon: BsTelephone,
-    route: "support",
+    route: SUPPORT_ROUTE,
   },
 ];
